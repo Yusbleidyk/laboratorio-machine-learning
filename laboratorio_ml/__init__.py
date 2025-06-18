555b93a7f2be782b1d1c979aa4a2651b505f5e88
Yusbleidyk <121525320+Yusbleidyk@users.noreply.github.com>
2025-06-18 15:49:05 -0400

    Create __init__.py
    Este archivo indica que 'laboratorio_ml' es un paquete de Python

---
+++ laboratorio_ml/__init__.py
@@ -0,0 +1,6 @@
+# Este archivo indica que 'laboratorio_ml' es un paquete de Python.
+# También expone funciones clave para facilitar su importación desde otros módulos.
+
+from .train import train_model
+from .dataset import load_data
+from .features import balance_classes
